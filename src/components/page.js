fe9094c9aae4a4cd667a38f053f38e21076ffbe8
vivek-khatri <veekik196@gmail.com>
2023-06-18 23:44:48 +0530

    Third Commit : Addinf Address details

--- src/components/page.js
+++ src/components/page.js
@@ -3,8 +3,9 @@ export default function Page() {
         <>
             <h1>Hello Friends</h1>
             <h3>This is the start of our Online Portal</h3>
-            <h5>We have our thela at Ramdaspeth, Nagpur where my father sells daily useful things like comb, hair clip, safety pins, cloth wasahing brush, tooth brush, soap tray, mug, bottle cleaner, tea filter, lemon squisher, Nylon ropes, elastic ropes, gas lighter, utensils cleaning net, mobile stand, scissors, etc.</h5>
+            <h5>We have our thela at Dharampeth, Nagpur where my father sells daily useful things like comb, hair clip, safety pins, cloth wasahing brush, tooth brush, soap tray, mug, bottle cleaner, tea filter, lemon squisher, Nylon ropes, elastic ropes, gas lighter, utensils cleaning net, mobile stand, scissors, etc.</h5>
             <br></br>
+            <h5><b>Address : </b> In Front of Lajree Matching Center, Khandelwal Jwellers, Priya Matching Center, Rajurkar Bandhu, Meena Bazaar, Manyavar Shwroom, Laxmi Bhawan Square, Dharampeth, Nagpur.</h5>
             <h5>All these things are sold of same quality in the shops at MRP.</h5>
             <h5>My father sells same quality material at less than shop's rate but with different brand of same quality which does not focuses on packaging.</h5>
             <h5>We use all those things at home that my father sells.</h5>
